518b34245f6881d1cab1d003692696577399f7c1
Arief Muarrief <arhief32@gmail.com>
2026-03-16 14:58:42 +0700

    v3

--- public/index.php
+++ public/index.php
@@ -2,50 +2,17 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
 
+/* middleware parsing body json */
 $app->addBodyParsingMiddleware();
 
-$app->get('/', function (Request $request, Response $response) {
+/* load helper */
+require __DIR__ . '/../src/helpers.php';
 
-    $data = [
-        "status" => "ok",
-        "message" => "Slim API running"
-    ];
-
-    $response->getBody()->write(json_encode($data));
-
-    return $response->withHeader('Content-Type', 'application/json');
-});
-
-$app->get('/api/market', function (Request $request, Response $response) {
-
-    $data = [
-        "symbol" => "BTCUSDT",
-        "price" => 65000
-    ];
-
-    $response->getBody()->write(json_encode($data));
-    return $response->withHeader('Content-Type', 'application/json');
-});
-
-
-$app->get('/api/trade/list', function (Request $request, Response $response) {
-
-    $data = [
-        [
-            "symbol" => "BTCUSDT",
-            "side" => "BUY",
-            "qty" => 0.01
-        ]
-    ];
-
-    $response->getBody()->write(json_encode($data));
-    return $response->withHeader('Content-Type', 'application/json');
-});
+/* load routes */
+(require __DIR__ . '/../src/routes.php')($app);
 
 $app->run();
